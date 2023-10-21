bb6b95e0bafcf6a106aa5a8275d3c1125d4f6a06
pinetreelch <chanho.lee0215@gmail.com>
2023-10-21 15:53:39 +0900

    ã프로젝트 만들기를 위한 준비: ì

--- src/main/java/me/pinetreelch/springbootdeveloper/MemberService.java
+++
@@ -1,2 +0,0 @@
-package me.pinetreelch.springbootdeveloper;public class MemberService {
-}
--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO Member (id, name) VALUES (1, 'name 1')
-INSERT INTO Member (id, name) VALUES (2, 'name 2')
-INSERT INTO Member (id, name) VALUES (3, 'name 3')
