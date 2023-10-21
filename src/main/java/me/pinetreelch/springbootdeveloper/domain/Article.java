20ea337ce1019d7efe7bb2152b6ff84c59986601
pinetreelch <chanho.lee0215@gmail.com>
2023-10-21 16:14:48 +0900

    빌더 활용해서 Article 앤티티 생성

---
+++ src/main/java/me/pinetreelch/springbootdeveloper/domain/Article.java
@@ -0,0 +1,52 @@
+package me.pinetreelch.springbootdeveloper.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GeneratedValue.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Builder // 필더 패턴임
+    public Article(String title, String content)
+    {
+        this.title      = title;
+        this.content    = content;
+    }
+
+    //빌더 패턴을 사용해서 필요 없어짐;
+//    protected Article(){ /* 기본 생성자 */ }
+//
+//    // Getter
+//    public long getId(){
+//        return id;
+//    }
+//
+//    public String getTitle(){
+//        return title;
+//    }
+//
+//    public String getContent(){
+//        return content;
+//    }
+
+
+}
